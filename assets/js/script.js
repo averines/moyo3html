7ca24ca75a35d404059a5b3192a59732567b904c
Admin <D:\[4EK.net]\XLAM\mail>
2021-05-21 22:04:26 +0300

    о компании + контакты

--- assets/js/script.js
+++ assets/js/script.js
@@ -652,3 +652,37 @@ if (tabsContainers.length > 0) {
 
     })
 }
+
+// Инициализация карты
+$(document).ready(function () {
+    "use strict";
+    ymaps.ready(init);
+    function init() {
+        let myMap;
+        if (!myMap) {
+            myMap = new ymaps.Map(
+                'js-map1',
+                {
+                    center: [55.77446156893533, 37.741998],
+                    zoom: 15
+                },
+                {
+                    searchControlProvider: 'yandex#search'
+                }
+            );
+            myMap.geoObjects.add(
+                new ymaps.Placemark(
+                    [55.77446156893533, 37.741998],
+                    {
+                        balloonContent: 'Окружной проезд, 30А',
+                        iconCaption: 'OptMoyo.ru'
+                    },
+                    {
+                        preset: 'islands#redDotIconWithCaption'
+                    }
+                )
+            );
+            myMap.behaviors.disable('scrollZoom')
+        }
+    }
+});
