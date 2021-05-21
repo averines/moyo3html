a802d4492658e65423703993b6124807482ad2c7
Admin <D:\[4EK.net]\XLAM\mail>
2021-05-21 19:02:07 +0300

    информационные страницы

--- assets/js/script.js
+++ assets/js/script.js
@@ -602,3 +602,53 @@ const removeIsActive = (items) => {
         item.classList.remove('is-active')
     })
 }
+
+//табы
+const tabsContainers = document.querySelectorAll('.tabs')
+
+if (tabsContainers.length > 0) {
+    tabsContainers.forEach(tabsContainer => {
+
+        let tabsContentItems = tabsContainer.querySelectorAll('.tabs-content__item')
+        let tabsTitlesItems = tabsContainer.querySelectorAll('.tabs-titles__item')
+
+        if (window.location.hash) {
+            let hash = window.location.hash
+
+            tabsTitlesItems.forEach((tabsTitlesItem, index) => {
+                if (tabsTitlesItem.hash == hash) {
+                    tabsTitlesItems[index].classList.add('is-active')
+                    tabsContentItems[index].classList.add('is-active')
+                    return
+                }
+            })
+        }
+        else {
+            tabsTitlesItems[0].classList.add('is-active')
+            tabsContentItems[0].classList.add('is-active')
+        }
+
+        tabsTitlesItems.forEach(tabsTitlesItem => {
+            tabsTitlesItem.addEventListener('click', (e) => {
+                e.preventDefault()
+
+                let activeContentTabId = tabsTitlesItem.getAttribute('href').split('#')[1]
+
+                //убираем активный класс со всех заголовков
+                tabsTitlesItems.forEach(item => { item.classList.remove('is-active') })
+
+                // делаем активным кликнутый заголовок
+                tabsTitlesItem.classList.add('is-active')
+
+                let activeContentTab = tabsContainer.querySelector(`[data-tabid="${activeContentTabId}"]`)
+
+                //убираем активный класс со всех элементов с контентом
+                tabsContentItems.forEach(item => { item.classList.remove('is-active') })
+
+                // делаем активным нужный контейнер с контентом
+                activeContentTab.classList.add('is-active')
+            })
+        })
+
+    })
+}
