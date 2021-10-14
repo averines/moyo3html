f71613749e50a405b021c51e9f56233d00e1cae1
Averines <averines@gmail.com>
2021-10-14 11:41:39 +0300

    js разбит на js и jq. изменен head

--- assets/js/script-js.js
+++ assets/js/script-js.js
@@ -321,8 +321,6 @@ for (let accordion of accordions) {
         accordionTitle.addEventListener('click', (e) => {
             if (!e.target.classList.contains('is-disabled')) {
                 accordionItem.classList.toggle('is-active')
-            } else {
-                console.log('этот элемент аккордиона заблокирован!');
             }
         })
 
@@ -481,7 +479,7 @@ if (products.length > 0) {
             // console.log('меняю фотки');
             productColorPicIndex = productColorPicIndex == (picsArray.length - 1) ? 0 : productColorPicIndex + 1;
             swapProductPicTimer = setTimeout(function () {
-                console.log('сменил фотку');
+                // console.log('сменил фотку');
                 productPicPath = `./i/products/300/${product.dataset.productId}-${productColorActiveId}-${picsArray[productColorPicIndex]}-300`
                 productPicSource.setAttribute('srcset', productPicPath + '.webp')
                 productPicImg.setAttribute('src', productPicPath + '.jpg')
@@ -535,10 +533,8 @@ if (products.length > 0) {
                 swapProductSizes(productColorActiveId)
             })
 
-            //прекращение листания при отведении курсоа с thumb
+            //прекращение листания при отведении курсора с thumb
             productColor.addEventListener('mouseout', () => {
-                // console.log('ставлю первую фотку для ранее наведенного товара');
-                // console.log('прекращаю менять фотки');
                 clearTimeout(swapProductPicTimer);
                 swapProductSizes(productColorActiveId)
             })
@@ -1039,7 +1035,7 @@ if (tabsContainers.length > 0) {
                     activeContentTab.classList.add('is-active')
 
                 } else {
-                    console.log('нет решетки, клик работает как ссылка');
+                    // console.log('нет решетки, клик работает как ссылка');
                 }
             })
         })
@@ -1257,15 +1253,6 @@ if (formAddressRegistration) {
     })
 
 }
-
-// плавная прокрутка к якорю
-// $(function () {
-//     $("a[href^='#']").click(function () {
-//         var _href = $(this).attr("href");
-//         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-//         return false;
-//     });
-// });
 
 
 // mixitup
@@ -1582,8 +1569,6 @@ function checkOrdeiItemRowValue(ordeiItemRow) {
         orderItemRowValueTotal = orderItemRowValueTotal + parseInt(item)
     })
 
-    console.log(orderItemRowValues);
-
     if (orderItemRowValueTotal > 0) {
         return true
     } else {
@@ -1617,7 +1602,6 @@ if (quantityCounters.length > 0) {
                 orderProductRow.style.display = 'none';
                 if (ordeiItemRow) {
                     if (checkOrdeiItemRowValue(ordeiItemRow)) {
-                        console.log('ok');
                     } else {
                         ordeiItemRow.classList.add('is-empty')
                     }
@@ -1648,7 +1632,6 @@ if (orderItems.length > 0) {
             orderItemremoveBtn.addEventListener('click', () => {
                 orderItem.remove()
                 orderItemsQuantity--
-                console.log(orderItemsQuantity);
                 if (orderItemsQuantity == 0) {
                     window.location.href = 'personal-cart-empty.html';
                 }
@@ -1731,7 +1714,7 @@ if (starsLinesItems) {
     })
 }
 
-// // предупреждение о необходимости проверить адрес доставки при оформлении заказа
+// предупреждение о необходимости проверить адрес доставки при оформлении заказа
 // const deliveryTypeWarningItems = document.querySelectorAll('.js-delivery-type-warning .form-group')
 
 // if (deliveryTypeWarningItems.length > 0) {
@@ -1824,7 +1807,6 @@ if (mainSlider) {
     let doPrevStep = true
 
     swiperMainSliderPicture.on('slideNextTransitionStart', function () {
-        console.log(doNextStep);
         if (doNextStep) {
             swiperMainSliderContent.slideNext();
             doNextStep = false
@@ -1832,7 +1814,6 @@ if (mainSlider) {
     });
 
     swiperMainSliderPicture.on('slidePrevTransitionStart', function () {
-        console.log(doPrevStep);
         if (doPrevStep) {
             swiperMainSliderContent.slidePrev();
             doPrevStep = false
@@ -1840,7 +1821,6 @@ if (mainSlider) {
     });
 
     swiperMainSliderContent.on('slideNextTransitionEnd', function () {
-        console.log(doNextStep);
         if (doNextStep) {
             swiperMainSliderPicture.slideNext();
         } else {
@@ -1849,7 +1829,6 @@ if (mainSlider) {
     });
 
     swiperMainSliderContent.on('slidePrevTransitionEnd', function () {
-        console.log(doPrevStep);
         if (doPrevStep) {
             swiperMainSliderPicture.slidePrev();
         } else {
@@ -1910,291 +1889,3 @@ if (productBuy) {
         productBuyShowSizes.classList.toggle('is-active')
     })
 }
-
-
-// плавная прокрутка к якорю
-// на странице
-$("body").on('click', '[href*="#"]', function (e) {
-    var fixed_offset = 100;
-    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-    e.preventDefault();
-});
-
-// на другой странице
-let myHash = location.hash;
-location.hash = '';
-if (myHash[1] != undefined) { 
-    $('html, body').animate({ scrollTop: $(myHash).offset().top - 70 }, 500); //скроллим за полсекунды
-};
-
-//fancybox
-$('[data-fancybox="gallery-feedback"]').fancybox({
-    backFocus: false,
-    hash: false,
-    loop: false,
-    protect: false,
-    buttons: ["close"],
-    image: {
-        preload: false
-    },
-    animationDuration: 400,
-    animationEffect: "fade",
-    transitionDuration: 1400,
-    transitionEffect: "fade",
-    lang: "ru",
-    i18n: {
-        ru: {
-            CLOSE: "Закрыть",
-            NEXT: "Вперед",
-            PREV: "Назад",
-            ERROR: "К сожалению, запрошенный контент невозможно загрузить.<br/> Пожалуйста, попробуйте позже.",
-            THUMBS: "Превью"
-        }
-    }
-});
-//fancybox
-$('[data-fancybox="gallery-feedback"]').fancybox({
-    backFocus: false,
-    hash: false,
-    loop: false,
-    protect: false,
-    buttons: ["close"],
-    image: {
-        preload: false
-    },
-    animationDuration: 400,
-    animationEffect: "fade",
-    transitionDuration: 1400,
-    transitionEffect: "fade",
-    lang: "ru",
-    i18n: {
-        ru: {
-            CLOSE: "Закрыть",
-            NEXT: "Вперед",
-            PREV: "Назад",
-            ERROR: "К сожалению, запрошенный контент невозможно загрузить.<br/> Пожалуйста, попробуйте позже.",
-            THUMBS: "Превью"
-        }
-    }
-});
-
-
-// Инициализация карты
-$(document).ready(function () {
-    if (document.getElementById('js-map1')) {
-        "use strict";
-        ymaps.ready(init);
-        function init() {
-            let myMap;
-            if (!myMap) {
-                myMap = new ymaps.Map(
-                    'js-map1',
-                    {
-                        center: [55.77446156893533, 37.741998],
-                        zoom: 15
-                    },
-                    {
-                        searchControlProvider: 'yandex#search'
-                    }
-                );
-                myMap.geoObjects.add(
-                    new ymaps.Placemark(
-                        [55.77446156893533, 37.741998],
-                        {
-                            balloonContent: 'Окружной проезд, 30А',
-                            iconCaption: 'OptMoyo.ru'
-                        },
-                        {
-                            preset: 'islands#redDotIconWithCaption'
-                        }
-                    )
-                );
-                myMap.behaviors.disable('scrollZoom')
-            }
-        }
-    }
-});
-
-
-const mapModalBtns = document.querySelectorAll('[data-action="mapmodal"]')
-let myMapModal;
-mapModalBtns.forEach(mapModalBtn => {
-    mapModalBtn.addEventListener('click', () => {
-        if (document.getElementById('js-map2')) {
-            "use strict";
-            ymaps.ready(init);
-            function init() {
-                if (!myMapModal) {
-                    myMapModal = new ymaps.Map(
-                        'js-map2',
-                        {
-                            center: [55.77446156893533, 37.741998],
-                            zoom: 15
-                        },
-                        {
-                            searchControlProvider: 'yandex#search'
-                        }
-                    );
-                    myMapModal.geoObjects.add(
-                        new ymaps.Placemark(
-                            [55.77446156893533, 37.741998],
-                            {
-                                balloonContent: 'Окружной проезд, 30А',
-                                iconCaption: 'OptMoyo.ru'
-                            },
-                            {
-                                preset: 'islands#redDotIconWithCaption'
-                            }
-                        )
-                    );
-                    myMapModal.behaviors.disable('scrollZoom')
-                }
-            }
-        }
-    })
-})
-
-
-//мультибегунок
-var $rangeSlider = $('[data-action="range-slider"]'),
-    $inputFrom = $('[data-action="range-from"]'),
-    $inputTo = $('[data-action="range-to"]'),
-    rangeInstance,
-    rangeMin = 100,
-    rangeMax = 1000,
-    rangeFrom = 100,
-    rangeTo = 1000;
-
-if ($rangeSlider.length > 0) {
-    $rangeSlider.ionRangeSlider({
-        skin: "round",
-        type: "double",
-        min: rangeMin,
-        max: rangeMax,
-        from: 100,
-        to: 1000,
-        onStart: updateInputs,
-        onChange: updateInputs
-    })
-
-    rangeInstance = $rangeSlider.data("ionRangeSlider");
-
-    function updateInputs(data) {
-        from = data.from;
-        to = data.to;
-
-        $inputFrom.prop("value", from);
-        $inputTo.prop("value", to);
-    }
-
-    $inputFrom.on("input", function () {
-        var val = $(this).prop("value");
-
-        // validate
-        if (val < rangeMin) {
-            val = rangeMin;
-        } else if (val > rangeTo) {
-            val = rangeTo;
-        }
-
-        rangeInstance.update({
-            from: val
-        });
-    })
-
-    $inputTo.on("input", function () {
-        var val = $(this).prop("value");
-
-        // validate
-        if (val < rangeFrom) {
-            val = rangeFrom;
-        } else if (val > rangeMax) {
-            val = rangeMax;
-        }
-
-        rangeInstance.update({
-            to: val
-        });
-    })
-}
-
-$('[data-fancybox="product-gallery"]').fancybox({
-    protect: true,
-    backFocus: false,
-    loop: true,
-    buttons: [
-        "zoom",
-        //"share",
-        // "slideShow",
-        //"fullScreen",
-        //"download",
-        "thumbs",
-        "close"
-    ],
-});
-
-// новый слайдер в модальном окне фансибокс
-$(".product__quick-view").fancybox({
-    afterShow: function (instance, slide) {
-        if (document.querySelectorAll('.product-gallery').length > 0) {
-            const swiperProductThumbs = new Swiper('.product-thumbs', {
-                direction: 'vertical',
-                loop: true,
-                grabCursor: false,
-                slidesPerView: 4,
-                setWrapperSize: true,
-                spaceBetween: 4,
-                allowTouchMove: false,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            })
-
-            const swiperProductGallery = new Swiper('.product-gallery-slider', {
-                loop: true,
-                grabCursor: true,
-                slidesPerView: 2,
-                setWrapperSize: true,
-                spaceBetween: 10,
-                navigation: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-                breakpoints: {
-                    768: {
-                        // allowTouchMove: false,
-                        slidesPerView: 1,
-                        spaceBetween: 0,
-                        grabCursor: false,
-                        speed: 0,
-                    }
-                }
-            })
-
-            const swiperProductThumbsItems = document.querySelectorAll('.product-thumbs__item')
-
-            swiperProductThumbsItems.forEach(item => {
-                item.addEventListener('mouseover', () => {
-                    swiperProductGallery.slideTo(parseInt(item.dataset.swiperSlideIndex) + 1);
-                })
-            })
-        }
-    }
-});
-
-
-window.addEventListener('resize', () => {
-    clientWidth = document.body.clientWidth;
-    // console.log(clientWidth);
-
-    if (clientWidth < 1024) {
-        // $("[data-fancybox='modal']").fancybox.close();
-        $.fancybox.close();
-    }
-}, false);
