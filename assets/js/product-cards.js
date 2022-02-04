fee30999fa945c874b2e261e666bfbcd1afcad94
Averines <averines@gmail.com>
2022-02-04 18:53:06 +0300

    смена фото в мини-карточке товара

--- assets/js/product-cards.js
+++ assets/js/product-cards.js
@@ -5,15 +5,17 @@
 const productsCards = document.querySelectorAll('.product-card');
 if (productsCards.length > 0) {
     productsCards.forEach(productsCard => {
-        let productPic = productsCard.querySelector('.product-card__pic');
+        let productWrapper = productsCard.querySelector('.product-card__wrapper');
         let productPicSources = productsCard.querySelectorAll('.product-card__pic source');
         let productPicImg = productsCard.querySelector('.product-card__pic img');
         let productColors = productsCard.querySelectorAll('.color-variants__item');
         let productSizes = productsCard.querySelectorAll('.size-variants__item');
         let productColorPicIndex = 0;
-        let swapProductPicTimer;
         let productColorActiveId;
+        let swapProductPicTimer;
         let colorPics;
+        let productPicPath0;
+        let productPicPath1;
 
         if (productColors.length > 0) {
             productColorActiveId = productColors[0].dataset.colorId;
@@ -22,15 +24,82 @@ if (productsCards.length > 0) {
 
         function swapProductPic(picsArray, productColorActiveId) {
             productColorPicIndex = productColorPicIndex == (picsArray.length - 1) ? 0 : productColorPicIndex + 1;
+
             swapProductPicTimer = setTimeout(function () {
-                let productPicPath0 = `./i/products/470/${productsCard.dataset.productId}-${productColorActiveId}-${picsArray[productColorPicIndex]}-470`;
-                let productPicPath1 = `./i/products/300/${productsCard.dataset.productId}-${productColorActiveId}-${picsArray[productColorPicIndex]}-300`;
+                if (picsArray[productColorPicIndex] == productColorActiveId) {
+                    productPicPath0 = `https://cdn2.moyo.moda/ws/main/900x1350/${picsArray[productColorPicIndex]}`;
+                    productPicPath1 = `https://cdn2.moyo.moda/ws/main/320x480/${picsArray[productColorPicIndex]}`;
+                } else {
+                    productPicPath0 = `https://cdn2.moyo.moda/ws/extra/900x1350/${picsArray[productColorPicIndex]}`;
+                    productPicPath1 = `https://cdn2.moyo.moda/ws/extra/320x480/${picsArray[productColorPicIndex]}`;
+                }
+
                 productPicSources[0].setAttribute('srcset', productPicPath0 + '.webp');
                 productPicSources[1].setAttribute('srcset', productPicPath1 + '.webp');
                 productPicImg.setAttribute('src', productPicPath0 + '.jpg');
                 swapProductPic(picsArray, productColorActiveId);
             }, 1000);
         }
+
+
+        //листание фото товара в мини-карточке товара при наведении на фото
+        productWrapper.addEventListener('mouseover', () => {
+            swapProductPic(colorPics, productColorActiveId);
+        });
+
+
+        //прекращение листания при отведении курсора с фото
+        productWrapper.addEventListener('mouseout', () => {
+            // productColorActiveId = productColors[0].dataset.colorId
+            productColorPicIndex = 0;
+
+            let productPicPath0 = `https://cdn2.moyo.moda/ws/main/900x1350/${productsCard.dataset.productId}`;
+            let productPicPath1 = `https://cdn2.moyo.moda/ws/main/320x480/${productsCard.dataset.productId}`;
+
+            productPicSources[0].setAttribute('srcset', productPicPath0 + '.webp');
+            productPicSources[1].setAttribute('srcset', productPicPath1 + '.webp');
+            productPicImg.setAttribute('src', productPicPath0 + '.jpg');
+
+            clearTimeout(swapProductPicTimer);
+            if (productSizes.length > 0) {
+                swapProductSizes(productColorActiveId);
+            }
+        });
+
+        //листание фото товара в мини-карточке товара при наведении на thumb
+        productColors.forEach(productColor => {
+            productColor.addEventListener('mouseover', () => {
+                productColorActiveId = productColor.dataset.colorId;
+                colorPics = productColor.dataset.colorPics.replace(/\s/g, '').split(',');
+
+                productPicPath0 = `https://cdn2.moyo.moda/ws/main/900x1350/${productColorActiveId}`;
+                productPicPath1 = `https://cdn2.moyo.moda/ws/main/320x480/${productColorActiveId}`;
+
+                productPicSources[0].setAttribute('srcset', productPicPath0 + '.webp');
+                productPicSources[1].setAttribute('srcset', productPicPath1 + '.webp');
+                productPicImg.setAttribute('src', productPicPath0 + '.jpg');
+
+                swapProductPic(colorPics, productColorActiveId);
+                if (productSizes.length > 0) {
+                    swapProductSizes(productColorActiveId);
+                }
+
+                //подстветка активного пункта
+                productColors.forEach(item => {
+                    item.classList.remove('is-active');
+                });
+                productColor.classList.add('is-active');
+            });
+
+            //прекращение листания при отведении курсора с thumb
+            productColor.addEventListener('mouseout', () => {
+                clearTimeout(swapProductPicTimer);
+                if (productSizes.length > 0) {
+                    swapProductSizes(productColorActiveId);
+                }
+            });
+        });
+
 
         //смена доступных размеров при наведении на thumb
         let swapProductSizes;
@@ -45,50 +114,5 @@ if (productsCards.length > 0) {
                 });
             };
         }
-
-        //листание фото товара в мини-карточке товара при наведении на фото
-        productPic.addEventListener('mouseover', () => {
-            swapProductPic(colorPics, productColorActiveId);
-        });
-
-        //прекращение листания при отведении курсора с фото
-        productPic.addEventListener('mouseout', () => {
-            // productColorActiveId = productColors[0].dataset.colorId
-            productColorPicIndex = 0;
-            let productPicPath0 = `./i/products/470/${productsCard.dataset.productId}-${productColorActiveId}-pic1-470`;
-            let productPicPath1 = `./i/products/300/${productsCard.dataset.productId}-${productColorActiveId}-pic1-300`;
-            productPicSources[0].setAttribute('srcset', productPicPath0 + '.webp');
-            productPicSources[1].setAttribute('srcset', productPicPath1 + '.webp');
-            productPicImg.setAttribute('src', productPicPath0 + '.jpg');
-
-            clearTimeout(swapProductPicTimer);
-            swapProductSizes(productColorActiveId);
-        });
-
-        productColors.forEach(productColor => {
-            //листание фото товара в мини-карточке товара при наведении на thumb
-            productColor.addEventListener('mouseover', () => {
-                productColorActiveId = productColor.dataset.colorId;
-                let productPicPath0 = `./i/products/470/${productsCard.dataset.productId}-${productColorActiveId}-pic1-470`;
-                let productPicPath1 = `./i/products/300/${productsCard.dataset.productId}-${productColorActiveId}-pic1-300`;
-                productPicSources[0].setAttribute('srcset', productPicPath0 + '.webp');
-                productPicSources[1].setAttribute('srcset', productPicPath1 + '.webp');
-                productPicImg.setAttribute('src', productPicPath0 + '.jpg');
-                swapProductPic(colorPics, productColorActiveId);
-                swapProductSizes(productColorActiveId);
-
-                //подстветка активного пункта
-                productColors.forEach(item => {
-                    item.classList.remove('is-active');
-                });
-                productColor.classList.add('is-active');
-            });
-
-            //прекращение листания при отведении курсора с thumb
-            productColor.addEventListener('mouseout', () => {
-                clearTimeout(swapProductPicTimer);
-                swapProductSizes(productColorActiveId);
-            });
-        });
     });
 }
