998452be90f03bc1c440d86c4141d8ee7f42cba7
Averines <averines@mail.ru>
2023-11-17 17:52:31 +0300

    upd: по рекомендациям pagespeed

--- assets/js/product-table.js
+++ assets/js/product-table.js
@@ -3,17 +3,17 @@
 // показать/скрыть все размеры в таблице товара, если размеров больше 8
 const productTableWrapper = document.querySelector('.product-table-wrapper');
 const productTableBtn = document.querySelector('.product-table__btn');
-const productsTableRows = document.querySelectorAll('.product-table__row');
-if (productsTableRows.length > 8) {
+// const productsTableRows = document.querySelectorAll('.product-table__row');
+// if (productsTableRows.length > 8) {
     if (productTableWrapper && productTableBtn) {
         productTableBtn.addEventListener('click', () => {
             productTableBtn.classList.toggle('is-active');
             productTableWrapper.classList.toggle('is-active');
         });
     }
-} else {
-    if (productTableWrapper && productTableBtn) {
-        productTableWrapper.classList.remove('product-table-wrapper');
-        productTableBtn.style.display = 'none';
-    }
-}
+// } else {
+//     if (productTableWrapper && productTableBtn) {
+//         productTableWrapper.classList.remove('product-table-wrapper');
+//         productTableBtn.style.display = 'none';
+//     }
+// }
