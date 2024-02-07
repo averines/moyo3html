0a116aaff771feed193d9a7a4cfeffadc91865e0
Averines <averines@mail.ru>
2024-02-07 20:08:03 +0300

    add: checkbox in cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -30,6 +30,7 @@ if (cartProductItems.length > 0) {
     });
 }
 
+
 // имитация применения промокода и бонуса в корзине
 const cartPromocodeBtn = document.querySelector('[data-action="promocode-btn"]');
 if (cartPromocodeBtn) {
@@ -43,4 +44,52 @@ if (cartBonusBtn) {
     cartBonusBtn.addEventListener('click', () => {
         cartBonusBtn.classList.add('is-active');
     });
+}
+
+
+window.addEventListener('click', (e) => {
+    if (e.target.dataset.hasOwnProperty('action')) {
+        if (e.target.dataset.action === 'toggle-products-in-cart') {
+            let productsCheckboxes = document.querySelectorAll('[data-action="toggle-product-in-cart"]');
+            if (productsCheckboxes.length > 0) {
+                let allChecked = true;
+                productsCheckboxes.forEach(productCheckbox => {  // проверяем, что все чекбоксы активны
+                    if (!productCheckbox.checked) { allChecked = false; }
+                });
+
+                if (allChecked) {
+                    productsCheckboxes.forEach(productCheckbox => { // если все чекбоксы активны, то отключаем
+                        productCheckbox.checked = false;
+                        e.target.classList.remove('is-active');
+                    });
+                } else {
+                    productsCheckboxes.forEach(productCheckbox => { // если хотя бы один чекбокс не активен, то включаем все
+                        productCheckbox.checked = true;
+                        e.target.classList.add('is-active');
+                    });
+                }
+
+                setCheckedProductsinCartCount(document.querySelectorAll('[data-action="toggle-product-in-cart"]:checked').length);
+            }
+        }
+
+        if (e.target.dataset.action === 'toggle-product-in-cart') {
+            let controlBtn = document.querySelector('[data-action="toggle-products-in-cart"]');
+            if (e.target.checked == false) {
+                controlBtn.classList.remove('is-active');
+            } else {
+                let productsCheckboxes = document.querySelectorAll('[data-action="toggle-product-in-cart"]');
+                let allChecked = true;
+                productsCheckboxes.forEach(productCheckbox => { if (!productCheckbox.checked) { allChecked = false; } });
+                if (allChecked) { controlBtn.classList.add('is-active'); }
+            }
+            setCheckedProductsinCartCount(document.querySelectorAll('[data-action="toggle-product-in-cart"]:checked').length);
+        }
+
+    }
+});
+
+function setCheckedProductsinCartCount(count) {
+    let el = document.querySelector('.cart-control__content>span');
+    if (el) { el.innerText = count }
 }
